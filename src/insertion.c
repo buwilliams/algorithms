890605f914ee45ece34c6b4dd6569395d37fe278
Buddy Williams <buddywilliams@gmail.com>
2014-03-11 09:45:08 -0400

    converted tabs to spaces

--- src/insertion.c
+++ src/insertion.c
@@ -6,60 +6,60 @@
 #include "insertion.h" 
 
 int main(int argc, char *argv[]) {
-	if(argc != 2) {
-		printf("Usage: ./insertion [size]\n");
-		return 0;
-	}
+  if(argc != 2) {
+    printf("Usage: ./insertion [size]\n");
+    return 0;
+  }
 
-	int size = atoi(argv[1]);
-	int *list = getListOfInts(size);
-	
-	printf("Sorted List:\n");
-	printList(list, size);
-	
-	shuffle(list, size);
-	printf("Shuffled List:\n");
-	printList(list, size);
+  int size = atoi(argv[1]);
+  int *list = getListOfInts(size);
+  
+  printf("Sorted List:\n");
+  printList(list, size);
+  
+  shuffle(list, size);
+  printf("Shuffled List:\n");
+  printList(list, size);
 
-	insertionSort(list, size);
-	printf("Insertion Sorted List:\n");
-	printList(list, size);
-	
-	free(list);
+  insertionSort(list, size);
+  printf("Insertion Sorted List:\n");
+  printList(list, size);
+  
+  free(list);
 }
 
 void insertionSort(int *list, int size) {
-	int i, n;
-	for(i=0; i<size; i++) { // loop through unsorted
-		if(i == 0) { continue; }
-		for(n=0; n<=i; n++) { // loop through sorted
-			if(*(list + i) <= *(list + n)) {
-				break;
-			}
-		}
-		swapWithShift(i, n, list);
-	}
+  int i, n;
+  for(i=0; i<size; i++) { // loop through unsorted
+    if(i == 0) { continue; }
+    for(n=0; n<=i; n++) { // loop through sorted
+      if(*(list + i) <= *(list + n)) {
+        break;
+      }
+    }
+    swapWithShift(i, n, list);
+  }
 }
 
 void swapWithShift(int fromIndex, int toIndex, int *list) {
-	int cacheValue = *(list + fromIndex);
-	if(fromIndex > toIndex) {
-		shiftRight(fromIndex, toIndex, list);
-	} else {
-		shiftLeft(fromIndex, toIndex, list);
-	}
-	*(list + toIndex) = cacheValue;
+  int cacheValue = *(list + fromIndex);
+  if(fromIndex > toIndex) {
+    shiftRight(fromIndex, toIndex, list);
+  } else {
+    shiftLeft(fromIndex, toIndex, list);
+  }
+  *(list + toIndex) = cacheValue;
 }
 
 void shiftRight(int fromIndex, int toIndex, int *list) {
-	for(int i=fromIndex; i>toIndex; i--) {
-		if(fromIndex == 0) return;
-		*(list + i) = *(list + (i-1));
-	}
+  for(int i=fromIndex; i>toIndex; i--) {
+    if(fromIndex == 0) return;
+    *(list + i) = *(list + (i-1));
+  }
 }
 
 void shiftLeft(int fromIndex, int toIndex, int *list) {
-	for(int i=fromIndex; i<toIndex; i++) {
-		*(list + i) = *(list + (i+1));
-	}
+  for(int i=fromIndex; i<toIndex; i++) {
+    *(list + i) = *(list + (i+1));
+  }
 }
--- src/utils.c
+++ src/utils.c
@@ -4,34 +4,34 @@
 #include <time.h>
 
 int *getListOfInts(int size) {
-	// Create a dynamically allocated array
-	int *list = malloc(sizeof(int) * size);
+  // Create a dynamically allocated array
+  int *list = malloc(sizeof(int) * size);
 
-	// Initialize the values in the dynamic array
-	for(int n=0; n<size; n++)
-		*(list + n) = n + 1;
+  // Initialize the values in the dynamic array
+  for(int n=0; n<size; n++)
+    *(list + n) = n + 1;
 
-	// return the pointer
-	return list;
+  // return the pointer
+  return list;
 }
 
 void printList(int *list, int size) {
-	for(int n=0; n<size; n++)
-		printf("%d\n", *(list + n));
+  for(int n=0; n<size; n++)
+    printf("%d\n", *(list + n));
 }
 
 // Shuffle algorithm
 void shuffle(int *list, int size) {
 
-	// Put seed in randomizer
-	srand(time(NULL));
+  // Put seed in randomizer
+  srand(time(NULL));
 
-	// walk through the size of the array
-	// and randomly swap it with other values
-	for(int i=0; i<size; i++) {
-		int randomNumber = rand() % size;
-		int cachedNumber = *(list + i);
-		*(list + i) = *(list + randomNumber);
-		*(list + randomNumber) = cachedNumber;
-	}
+  // walk through the size of the array
+  // and randomly swap it with other values
+  for(int i=0; i<size; i++) {
+    int randomNumber = rand() % size;
+    int cachedNumber = *(list + i);
+    *(list + i) = *(list + randomNumber);
+    *(list + randomNumber) = cachedNumber;
+  }
 }
